78568ea6c5165b9054bf598842e50d9e695a690e
Herbert Ludowieg <herbertl@buffalo.edu>
2017-04-26 15:20:14 -0400

    It was succesful and will now be merged with optical branch and another will be made to see if the temp vs density looks better without the b value.

--- optical-pumping/scripts/expo_plot.py
+++ optical-pumping/scripts/expo_plot.py
@@ -5,48 +5,63 @@ from scipy.optimize import curve_fit
 import numpy as np
 from _sig_fig import *
 
-def func(x,a,b,k,z):
-  return a*np.exp(-k*(x-z))+b
+def light_func(x,a,b,k):
+  return a*np.exp(-k*(x-4e16))+b
+
+def dens_func(x,a,b,k):
+  return a*np.exp(k*(x-87))+b
 
 # Finds exponential fit to a*e^(-k*(x-z))+b
-def exponential_4(x,y,p0,sigma=0):
+def exponential_3(x,y,p0,func,sigma=0):
   #print x,y,p0
   if sigma == 0:
     param,pcov = curve_fit(func,x,y,p0)
   else:
     param,pcov = curve_fit(func,x,y,p0,sigma=sigma)
+  print "==========BEGIN FIT DATA========================"
   print "Initial guess parameters:"
   print "a = "+str(p0[0])
   print "b = "+str(p0[1])
   print "k = "+str(p0[2])
-  print "z = "+str(p0[3])
   print "Calculated parameters with curve_fit function:"
-  print "Format (a,b,k,z)"
-  print param
+  print "Format (a,b,k)"
+  print param[0],param[1],param[2]
   print "Covariance matrix from curve_fit function:"
   print pcov
-  a,b,k,z = param
-  sigma_a,sigma_b,sigma_k,sigma_z = np.sqrt(abs(np.diag(pcov)))
+  a,b,k = param
+  sigma_a,sigma_b,sigma_k = np.sqrt(abs(np.diag(pcov)))
+  print "Sigma values:"
+  print "Format (a,b,k)"
+  print sigma_a,sigma_b,sigma_k
+  print "==========END FIT DATA=========================="
   round_a = sig_fig(sigma_a,a)
   round_b = sig_fig(sigma_b,b)
   round_k = sig_fig(sigma_k,k)
-  round_z = sig_fig(sigma_z,z)
-  return a,b,k,z,round_a,round_b,round_k,round_z
+  #round_z = sig_fig(sigma_z,z)
+  return a,b,k,round_a,round_b,round_k
 
-def open_file(fn):
-  x = []
-  y = []
-  for i in fn.readlines():
-    if i == "#":
-      continue
-    
+def temperature_vs_density(density):
+  x = np.zeros(len(density))
+  y = np.zeros(len(density))
+  for i in range(len(density)):
+    x[i] = density[i][0]
+    y[i] = density[i][1]
+  xx = np.linspace(x[0],x[-1],100)
+  n,m,l = 1e18,8.5e-2,7e15
+  yy = dens_func(xx,n,l,m)
+  a,b,k,round_a,round_b,round_k = exponential_3(x,y,(n,l,m),dens_func)
+  yyy = dens_func(xx,a,b,k)
+  ax,bx = plt.subplots(1)
+  bx.plot(x,y,'ro')
+  bx.plot(xx,yy,'b-')
+  bx.plot(xx,yyy,'r-')
+  ax.show()
 
-def temp(density):
+def density_vs_light(density):
   data_file = open("../i/data","r")
   data = []
   x = []
   y = []
-
   for i in data_file.readlines():
     if i[0] == "#":
       continue
@@ -78,17 +93,30 @@ def temp(density):
   for i in range(len(data)):
     x[i] = density_slim[i][1]
     y[i] = data[i][1]
-  xx = np.linspace(-4.0e18,4.0e18,1000)
-  yy = func(xx,10,1,6e-18,2e16)
+  xx = np.linspace(x[0],x[-1],1000)
+  yy = light_func(xx,10,1,6e-18)
   #yy = func(xx,12.8356290,1.25988601,6.08534284e-18,-4.93643723e16)
   #print yy
-  a,b,k,z,round_a,round_b,round_k,round_z = exponential_4(x,y,(10,1,6e-18,2e16),0.002)
-  #print round_a,round_b,round_k,round_z
-  yyy = func(xx,a,b,k,z)
+  print "*************************************************"
+  print "Fit for light intensity as a function of density"
+  a,b,k,round_a,round_b,round_k = exponential_3(x,y,(10,1,6e-18),light_func,0.002)
+  print "\nFit parameters with uncertainties:"
+  print "a = "+str(round_a[1])+" +/- "+str(round_a[0])
+  print "b = "+str(round_b[1])+" +/- "+str(round_b[0])
+  print "k = "+str(round_k[1])+" +/- "+str(round_k[0])
+  print "z = 4e16"
+  print "*************************************************"
+  yyy = light_func(xx,a,b,k)
   ax,bx = plt.subplots(1)
-  bx.plot(x,y,'ro',xx,yyy,'r-',xx,yy,'b-')
-  bx.set_ylim([0,12])
-  bx.set_xlim([-4.0e18,4.0e18])
+  bx.plot(x,y,'ro',label="Data")
+  bx.plot(xx,yyy,'r-',label='Fit')
+  bx.plot(xx,yy,'b-',label='Guess Fit')
+  bx.set_xlabel("Density ($m^{-3}$)")
+  bx.set_ylabel("Light Intensity ($V$)")
+  bx.set_title("Density vs. Light Intensity")
+  bx.legend()
+  #bx.set_ylim([0,12])
+  #bx.set_xlim([4.0e18])
   ax.show()
   
 
@@ -107,7 +135,8 @@ def main():
     density.append([])
     density[i].append(x[i])
     density[i].append(y[i])
-  temp(density)
+  density_vs_light(density)
+  temperature_vs_density(density)
   raw_input()
 
 main()
